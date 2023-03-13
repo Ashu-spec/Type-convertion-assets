5a8291e723ef05cefee3c6dc8dc6e2d9cee00ccf
Ashu-Specs <christianashish050@gmail.com>
2023-03-13 12:12:58 +0530

    gess_planet

--- 3/gess_planet.py
+++ 3/gess_planet.py
@@ -2,8 +2,11 @@ correct_guess: bool = False
 guess: str = ""
 planet: str = "Zorten"
 
-guess = input("Louis Asked can you guess his planet's name? >>> ")
+while correct_guess is not True:
+  guess = input("Louis Asked can you guess his planet's name? >>> ")
 
-print(guess)
-
-
+  if guess.lower() == planet.lower():
+      print("Right Guess!! Louis is From Zortan ")
+      correct_guess = True
+  else:
+      print("Wrong Choice, try again!")
